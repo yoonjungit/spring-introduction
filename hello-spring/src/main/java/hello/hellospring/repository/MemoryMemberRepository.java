e4653d2ed25d00d652a31946d403e3ed7b58ad68
Yoonjung Yim <yoonjung623@naver.com>
2023-01-12 19:52:59 +0900

    Feat : 컴포넌트 스캔과 자동 의존관계 설정
    회원 컨트롤러가 회원 서비스 및 회원 리포지토리를 사용할 수 있도록 의존관계를 설정

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -4,6 +4,9 @@ import hello.hellospring.domain.Member;
 
 import java.util.*;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
     private static Map<Long, Member> store = new HashMap<>() ;
     private static long sequence = 0L ;
